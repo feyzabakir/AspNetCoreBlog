c6419efa046d33de33b6368e0efd3daecba6b693
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2023-11-28 10:28:45 +0300

    #124. Ders Sonu

---
+++ CoreDemo/Models/UserSignUpViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreDemo.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name ="Ad Soyad")]
+        [Required(ErrorMessage ="Lütfen Ad Soyad Giriniz")]
+        public string NameSurname { get; set; }
+
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz.")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz.")]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre Tekrarı")]
+        [Compare("Password", ErrorMessage = "Şifreleriniz uyuşmuyor.")]
+        public string ConfirimPassword { get; set; }
+
+        [Display(Name = "E-Posta")]
+        [Required(ErrorMessage = "Lütfen e-posta adresinizi giriniz.")]
+        public string Mail { get; set; }
+
+    }
+}

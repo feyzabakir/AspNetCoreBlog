fa53b84ab2f74c473e5304d1e62495f1a2c0f702
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2023-11-29 22:20:51 +0300

    #128.Ders Sonu

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -14,16 +14,20 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
+    [AllowAnonymous]
     public class BlogController : Controller
     {
         BlogManager bm = new BlogManager(new EfBlogRepository());
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
         Context c = new Context();
+
+
         public IActionResult Index()
         {
             var values = bm.GetBlogListWithCategory();
             return View(values);
         }
+
         public IActionResult BlogReadAll(int id)
         {
             ViewBag.i = id;
--- CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
@@ -9,15 +11,19 @@ namespace CoreDemo.ViewComponents.Writer
 {
     public class WriterAboutOnDashboard:ViewComponent
     {
-        WriterManager wm = new WriterManager(new EfWriterRepository());
-        Context c = new Context();
+		WriterManager writermanager = new WriterManager(new EfWriterRepository());
 
-        public IViewComponentResult Invoke()
-        {
-            var usermail = User.Identity.Name;
-            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
-            var values = wm.GetWriterByID(writerID);
-            return View(values);
-        }
-    }
+		Context c = new Context();
+
+		public IViewComponentResult Invoke()
+		{
+			//var user = await _userManager.FindByNameAsync(User.Identity.Name);
+			var username = User.Identity.Name;
+			ViewBag.v = username;
+			var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+			var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+			var values = writermanager.GetWriterByID(writerID);
+			return View(values);
+		}
+	}
 }
